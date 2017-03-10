003e841c6c3cf0dd7e9f312746710ee0ca918020
Benedikt Seidl <benedikt.seidl@tum.de>
2017-03-10 16:58:52 +0100

    Depot test

--- depot/tests.py
+++ depot/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from .models import Depot
 
-# Create your tests here.
+class DepotTestCase(TestCase):
+
+    def test_str(self):
+        depot = Depot(1, "My depot")
+        self.assertEqual(depot.__str__(), "Depot My depot")
