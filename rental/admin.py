6eee71797c3bcd68142df27816b93c34d4926717
Benedikt Seidl <benedikt.seidl@tum.de>
2017-03-13 15:37:38 +0100

    Create rental model
    Also has an admin interface now

--- rental/admin.py
+++ rental/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Rental, ItemRental
+
+class ItemRentalInline(admin.TabularInline):
+    model = ItemRental
+    extra = 0
+
+class RentalAdmin(admin.ModelAdmin):
+    inlines = [ItemRentalInline]
+
 
 # Register your models here.
+admin.site.register(Rental, RentalAdmin)
--- rental/models.py
+++ rental/models.py
@@ -1,3 +1,49 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import User
+from depot.models import Item
 
-# Create your models here.
+class Rental(models.Model):
+    """
+    A rental defines the amount of items and has a start and return date.
+
+    :author: Benedikt Seidl
+    """
+
+    STATE_PENDING = '1'
+    STATE_APPROVED = '2'
+    STATE_DECLINED = '3'
+    STATE_REVOKED = '4'
+    STATES = (
+        (STATE_PENDING, 'pending'),
+        (STATE_APPROVED, 'approved'),
+        (STATE_DECLINED, 'declined'),
+        (STATE_REVOKED, 'revoked'),
+    )
+
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    items = models.ManyToManyField(Item, through='ItemRental')
+    name = models.CharField(max_length=256)
+    email = models.EmailField()
+    purpose = models.CharField(max_length=256, blank=True)
+    user = models.ForeignKey(User, blank=True, null=True)
+    start_date = models.DateTimeField()
+    return_date = models.DateTimeField()
+    state = models.CharField(max_length=1, choices=STATES, default=STATE_PENDING)
+
+    def __str__(self):
+        return 'Rental by %s' % self.name
+
+
+class ItemRental(models.Model):
+    """
+    Intermediate relationship for each item within a rental.
+    Defines the quantity and number of returned items.
+
+    :author: Benedikt Seidl
+    """
+
+    rental = models.ForeignKey(Rental, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
+    returned = models.PositiveSmallIntegerField(default=0)
