a36033badfa90fde764b136fa1e713dbb267a02b
Benedikt Seidl <benedikt.seidl@tum.de>
2017-03-13 13:37:20 +0100

    Fix pylint complaining about spaces and other stuff

--- depot/admin.py
+++ depot/admin.py
@@ -13,7 +13,8 @@ class DepotAdmin(admin.ModelAdmin):
 
     actions = ["make_archived", "make_restored"]
 
-    def make_message(self, num_changed, change):
+    @staticmethod
+    def format_message(num_changed, change):
         if num_changed == 1:
             message = "1 depot was"
         else:
@@ -21,13 +22,13 @@ class DepotAdmin(admin.ModelAdmin):
         return "%s successfully marked as %s" % (message, change)
 
     def make_archived(self, request, queryset):
-        depots_archived = queryset.update(active = False)
-        self.message_user(request, self.make_message(depots_archived, "archived"))
+        depots_archived = queryset.update(active=False)
+        self.message_user(request, DepotAdmin.format_message(depots_archived, "archived"))
     make_archived.short_description = "Archive selected depots"
 
     def make_restored(self, request, queryset):
-        depots_restored = queryset.update(active = True)
-        self.message_user(request, self.make_message(depots_restored, "restored"))
+        depots_restored = queryset.update(active=True)
+        self.message_user(request, DepotAdmin.format_message(depots_restored, "restored"))
     make_restored.short_description = "Restore selected depots"
 
 # make depots modifiable by admin
