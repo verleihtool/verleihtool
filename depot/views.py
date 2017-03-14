f126a2af696ce0e98d47f2974f5afbf8d212c944
Florian Stamer <ga65zuy@mytum.de>
2017-03-14 14:07:44 +0100

    Created basic interface.
    Lists depots/items
    Renamed 'all' show to detail
    Moved query from detail.html to detail in depot/views.py
    Changed html Head for templates
    Shortend query for detail in depot/views.py

--- depot/views.py
+++ depot/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Depot, Item
 
 # Create your views here.
+
+def index(request):
+    depot_list = Depot.objects.all()
+    context = {
+        'depot_list': depot_list,
+    }
+    return render(request, 'depot/index.html', context)
+
+def detail(request, depot_id):
+    depot = get_object_or_404(Depot, pk=depot_id)
+    if request.user.is_authenticated:
+        item_list = depot.item_set.all()
+    else:
+        item_list = depot.item_set.filter(visibility=Item.VISIBILITY_PUBLIC)
+    context = {
+        'depot_name': depot.name,
+        'item_list': item_list,
+    }
+    return render(request, 'depot/detail.html', context)
