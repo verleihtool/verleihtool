974c57d873345ba3930fa07ad30392a6702e45a4
Benedikt Seidl <benedikt.seidl@tum.de>
2017-03-16 15:27:45 +0100

    Create custom base testcase class

---
+++ verleihtool/test.py
@@ -0,0 +1,44 @@
+from django.contrib.auth.models import User
+from django.test import TestCase as BaseTestCase, Client
+
+
+class TestCase(BaseTestCase):
+    """
+    Base test case with convenience methods to login and assert responses
+
+    :author: Benedikt Seidl
+    """
+
+    def setUp(self):
+        # Create normal user
+        User.objects.create_user(
+            username='user',
+            password='password'
+        )
+
+        # Create superuser
+        User.objects.create_superuser(
+            username='admin',
+            email='admin@example.com',
+            password='pass'
+        )
+
+    @property
+    def as_guest(self):
+        return Client()
+
+    @property
+    def as_user(self):
+        c = Client()
+        c.login(username='user', password='password')
+        return c
+
+    @property
+    def as_superuser(self):
+        c = Client()
+        c.login(username='admin', password='pass')
+        return c
+
+    def assertSuccess(self, response, template):
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, template)
