b8f3f523c0e475bafdb261e9b4c893fe754fea07
Benedikt Seidl <benedikt.seidl@tum.de>
2017-03-17 18:07:23 +0100

    Add verification to rental model

--- rental/tests.py
+++ rental/tests.py
@@ -1,7 +1,8 @@
 import datetime
 from django.test import TestCase
 from django.core.exceptions import ValidationError
-from .models import Rental
+from .models import Rental, ItemRental
+from depot.models import Item
 
 
 class RentalTestCase(TestCase):
@@ -31,3 +32,51 @@ class RentalTestCase(TestCase):
     def test_str(self):
         rental = Rental(name='Fitnessflow')
         self.assertEqual(rental.__str__(), 'Rental by Fitnessflow')
+
+
+class ItemRentalTestCase(TestCase):
+
+    def setUp(self):
+        self.rental = Rental(
+            start_date=datetime.datetime(2017, 3, 17),
+            return_date=datetime.datetime(2017, 3, 19)
+        )
+        self.item = Item(
+            name="My Item",
+            quantity=42
+        )
+
+    def test_clean__valid_data(self):
+        itemRental = ItemRental(
+            rental=self.rental,
+            item=self.item,
+            quantity=12
+        )
+        itemRental.clean()
+
+    def test_clean__negative_amount(self):
+        itemRental = ItemRental(
+            rental=self.rental,
+            item=self.item,
+            quantity=-1
+        )
+        with self.assertRaises(ValidationError):
+            itemRental.clean()
+
+    def test_clean__zero_amount(self):
+        itemRental = ItemRental(
+            rental=self.rental,
+            item=self.item,
+            quantity=0
+        )
+        with self.assertRaises(ValidationError):
+            itemRental.clean()
+
+    def test_clean__greater_amount(self):
+        itemRental = ItemRental(
+            rental=self.rental,
+            item=self.item,
+            quantity=56
+        )
+        with self.assertRaises(ValidationError):
+            itemRental.clean()
