eb81dd4b02ce3fa58f52a5cd4ec85bc77915e112
Benedikt Seidl <benedikt.seidl@tum.de>
2017-03-20 17:09:19 +0100

    Replace double with single quotes

--- depot/admins/depot.py
+++ depot/admins/depot.py
@@ -14,24 +14,24 @@ class DepotAdmin(admin.ModelAdmin):
     list_display = ['name', 'active']
     ordering = ['name']
 
-    actions = ["make_archived", "make_restored"]
+    actions = ['make_archived', 'make_restored']
 
     def format_message(self, num_changed, change):
         if num_changed == 1:
-            message = "1 depot was"
+            message = '1 depot was'
         else:
-            message = "%s depots were" % num_changed
-        return "%s successfully marked as %s" % (message, change)
+            message = '%s depots were' % num_changed
+        return '%s successfully marked as %s' % (message, change)
 
     def make_archived(self, request, queryset):
         depots_archived = queryset.update(active=False)
-        self.message_user(request, self.format_message(depots_archived, "archived"))
-    make_archived.short_description = "Archive selected depots"
+        self.message_user(request, self.format_message(depots_archived, 'archived'))
+    make_archived.short_description = 'Archive selected depots'
 
     def make_restored(self, request, queryset):
         depots_restored = queryset.update(active=True)
-        self.message_user(request, self.format_message(depots_restored, "restored"))
-    make_restored.short_description = "Restore selected depots"
+        self.message_user(request, self.format_message(depots_restored, 'restored'))
+    make_restored.short_description = 'Restore selected depots'
 
     def get_queryset(self, request):
         qs = super().get_queryset(request)
