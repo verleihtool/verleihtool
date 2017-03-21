34296c5800d6292e83fc75f9270feb8cf822a019
STFN <stfn.su@tum.de>
2017-03-21 15:03:40 +0100

    Fix lint fail

--- rental/views.py
+++ rental/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from .models import Rental, ItemRental
 from django.db import transaction
