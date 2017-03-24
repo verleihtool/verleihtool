9f2686b4e4379c9c29d5d3490531f1a99ef00c2b
Benedikt Seidl <benedikt.seidl@tum.de>
2017-03-24 22:20:33 +0100

    Add number input widget

---
+++ resources/js/number-input.js
@@ -0,0 +1,28 @@
+// Create our own number input spinner
+$('.number-up').click((ev) => {
+    let $el = $(ev.target).parents('.number').find('input')
+    let val = parseInt($el.val())
+
+    $el.val(val + 1).change()
+})
+
+$('.number-down').click((ev) => {
+    let $el = $(ev.target).parents('.number').find('input')
+    let val = parseInt($el.val())
+
+    $el.val(val - 1).change()
+})
+
+// Validate number inputs
+$('.number input').change((ev) => {
+    let val = parseInt($(ev.target).val())
+    let min = parseInt($(ev.target).attr('min'))
+    let max = parseInt($(ev.target).attr('max'))
+
+    if (!val || val < min) {
+        $(ev.target).val(min)
+    }
+    else if (val > max) {
+        $(ev.target).val(max)
+    }
+})
