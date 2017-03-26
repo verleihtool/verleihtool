7a580b235241585fc9d12d2796d04c57acece58c
Benedikt Seidl <benedikt.seidl@tum.de>
2017-03-26 12:09:12 +0200

    Reduce max line length to 100

--- depot/tests/test_checkout_form.py
+++ depot/tests/test_checkout_form.py
@@ -23,12 +23,14 @@ class AutoFillTestCase(ClientTestCase):
         response = self.as_user.get('/depots/%d/' % self.depot.id)
 
         self.assertInHTML(
-            '<input type="text" class="form-control" id="id_username" name="name" value="user" required>',
+            '<input type="text" class="form-control" id="id_username"'
+            'name="name" value="user" required>',
             response.content.decode()
         )
 
         self.assertInHTML(
-            '<input type="email" class="form-control" id="email" name="email" value="user@example.com" required>',
+            '<input type="email" class="form-control" id="email"'
+            'name="email" value="user@example.com" required>',
             response.content.decode()
         )
 
@@ -36,11 +38,13 @@ class AutoFillTestCase(ClientTestCase):
         response = self.as_guest.get('/depots/%d/' % self.depot.id)
 
         self.assertInHTML(
-            '<input type="text" class ="form-control" id="id_username" name="name" value="" required>',
+            '<input type="text" class ="form-control" id="id_username"'
+            'name="name" value="" required>',
             response.content.decode()
         )
 
         self.assertInHTML(
-            '<input type="email" class="form-control" id="email" name="email" value="" required>',
+            '<input type="email" class="form-control" id="email"'
+            'name="email" value="" required>',
             response.content.decode()
         )
