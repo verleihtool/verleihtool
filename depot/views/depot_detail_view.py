44eff2524449a42d9b58effc4c8c86cbf558164a
Benedikt Seidl <benedikt.seidl@tum.de>
2017-10-08 23:50:52 +0200

    Show availability for the next week as default

--- depot/views/depot_detail_view.py
+++ depot/views/depot_detail_view.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta
-from depot import helpers
+from depot import availability, helpers
 from django.shortcuts import render
 from django.views import View
 
@@ -17,9 +17,26 @@ class DepotDetailView(View):
     def get(self, request, depot_id):
         depot = helpers.get_depot_if_allowed(depot_id, request.user)
 
+        start_date = datetime.now()
+        return_date = datetime.now() + timedelta(days=7)
+
+        item_list = helpers.get_item_list(depot, request.user)
+        item_availability_intervals = availability.get_item_availability_intervals(
+            start_date, return_date, depot_id, item_list
+        )
+
+        availability_data = []
+        for item, intervals in item_availability_intervals:
+            availability_data.append((
+                item,
+                helpers.get_chart_data(intervals),
+                availability.get_minimum_availability(intervals)
+            ))
+
         return render(request, 'depot/detail.html', {
             'depot': depot,
-            'item_list': helpers.get_item_list(depot, request.user),
+            'item_list': item_list,
+            'availability_data': availability_data,
             'show_visibility': depot.show_private_items(request.user),
             'managed_by_user': depot.managed_by(request.user),
             'start_date': datetime.now() + timedelta(days=1),
