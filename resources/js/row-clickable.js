2537c86684ae6cd1a3cbdb718a3bf1fea2d97899
Benedikt Seidl <benedikt.seidl@tum.de>
2017-10-09 09:09:28 +0200

    Support Ctrl-click to mimic link behaviour

--- resources/js/row-clickable.js
+++ resources/js/row-clickable.js
@@ -1,7 +1,11 @@
-$('table.table-clickable tr.row-clickable').click(function () {
+$('table.table-clickable tr.row-clickable').click(function(ev) {
     let href = $(this).data('href')
 
     if (href) {
-        window.location.href = href
+        if (ev.ctrlKey || ev.metaKey) {
+            window.open(href, '_blank')
+        } else {
+            window.location.href = href
+        }
     }
 })
