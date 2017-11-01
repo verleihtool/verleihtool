316c9cc8b94eb8048ba25f1a734ad60c028a7946
STFN <stfn.su@tum.de>
2017-11-01 19:04:22 +0100

    Modify email sending within command that checks for due rentals

--- rental/management/commands/check_for_maturity.py
+++ rental/management/commands/check_for_maturity.py
@@ -13,14 +13,14 @@ class Command(BaseCommand):
     def get_dmg_emailaddr_list(depot_managers):
         email_list = []
         for dmg in depot_managers:
-            email_list += [dmg.email]
+            email_list.append(dmg.email)
         return email_list
 
-    @staticmethod
-    def get_mail_tuple(rental):
+    def get_mail_tuple(self, rental, interval):
         """
         Render the template for the rental and transform it to string tuple
         :param rental: the rental that is due
+        :param interval: number of days twithin which a reminder email is to be sent
         :return: an email string tuple (first component: email for requester,
         second component: email for dmg)
         """
@@ -38,38 +38,35 @@ class Command(BaseCommand):
         html_mail_to_requester = render_to_string('first-reminder-to-requester.html', mailcontext)
         html_mail_to_dmg = render_to_string('first-reminder-to-dmg.html', mailcontext)
         return (
-            html2text.html2text(html_mail_to_requester),
-            html2text.html2text(html_mail_to_dmg)
+            (
+                '[Verleihtool] Your rental request from "%s" is due for %i days, %s %s!'
+                % (rental.depot.name, interval, rental.firstname, rental.lastname),
+                html2text.html2text(html_mail_to_requester),
+                'verleih@fs.tum.de',
+                [rental.email]
+             ),
+            (
+                '[Verleihtool] Your rental request from "%s" is due for %i days, %s %s!'
+                % (rental.depot.name, interval, rental.firstname, rental.lastname),
+                html2text.html2text(html_mail_to_dmg),
+                'verleih@fs.tum.de',
+                self.get_dmg_emailaddr_list(rental.depot.managers)
+            ),
         )
 
-    def send_first_reminder(self):
-        today = datetime.today()
-        # todo set time interval externally by passing arguments to a method when setting cron job
-        interval = 7
-        due_since_interval_rentals = Rental.objects.filter(
-            return_date=today - timedelta(days=interval),
+    @staticmethod
+    def get_due_rentals(interval):
+        return Rental.objects.filter(
+            return_date=datetime.today - timedelta(days=interval),
             state='2'
         )
 
-        emails = ()
-
+    def send_first_reminder(self):
+        # todo set time interval externally by passing arguments to a method when setting cron job
+        interval = 7
+        due_since_interval_rentals = self.get_due_rentals(interval)
         for rental in due_since_interval_rentals:
-            plain_txt_mail_tuple = self.get_mail_tuple(rental)
-            emails += (
-                '[Verleihtool] Your rental request from "%s" is due since %i days, %s %s!'
-                % (rental.depot.name, interval, rental.firstname, rental.lastname),
-                plain_txt_mail_tuple[0],
-                'verleih@fs.tum.de',
-                rental.email
-            ) + (
-                '[Verleihtool] Rental request by %s %s from "%s" has been due since %i days!'
-                % (rental.firstname, rental.lastname, rental.depot.name, interval),
-                plain_txt_mail_tuple[1],
-                'verleih@fs.tum.de',
-                self.get_dmg_emailaddr_list(rental.depot.managers),
-            )
-
-        send_mass_mail(emails, fail_silently=True)
+            send_mass_mail(self.get_mail_tuple(rental, interval), fail_silently=True)
 
     def handle(self, *args, **options):
         self.send_first_reminder()
